a25d44a545f49519991c83c30516aa009a862d3d
YigitDemiralp <49249587+YigitDemiralp@users.noreply.github.com>
2021-02-12 15:00:00 -0500

    Create Phhase 2.R

---
+++ Phhase 2.R
@@ -0,0 +1,19 @@
+library(data.table)
+library(ggplot2)
+library(scales)
+
+df <- read.csv("C:/Users/YEET/Documents/BU/Classes/ba 810/Project/aug_train.csv")
+head(df)
+
+levels(df$last_new_job) <- c(levels(df$last_new_job), 0) 
+df$last_new_job[df$last_new_job  == 'never']  <- 0 
+df$last_new_job[df$last_new_job == ""]<-0
+
+head(df)
+
+unique(df['last_new_job'])
+
+
+df[which((df$company_type == 'Early Stage Startup') & (df$company_size == '10/49'))]
+
+table(df$last_new_job)
